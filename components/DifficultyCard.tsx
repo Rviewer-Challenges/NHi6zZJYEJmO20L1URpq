9b9a76ffec9b7ea290846994ab68a95f39586034
talha <atercan0658@gmail.com>
2023-10-07 17:08:53 +0200

    home page completed

--- components/DifficultyCard.tsx
+++ components/DifficultyCard.tsx
@@ -1,4 +1,5 @@
 import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
 
 interface Difficulty {
   id: string,
@@ -9,13 +10,15 @@ interface Difficulty {
 
 export default function DifficultyCard({ item }: { item: Difficulty }) {
   return (
-    <div id={item.id} className="flex flex-col justify-center text-center gap-y-1 text-[#EDBC06]">
-      <div className="w-48 h-48 square">
-        <Image
-          alt={item.name}
-          src={item.img}
-          className="w-full h-full"
-        />
+    <div id={item.id} className="flex flex-col justify-center text-center gap-y-1 text-[#EDBC06] relative">
+      <div className="w-48 h-48 card border-8 border-[#EDBC06]">
+        <Link href={item.url}>
+          <Image
+            alt={item.name}
+            src={item.img}
+            className="w-full h-full relative z-10"
+          />
+        </Link>
       </div>
       <h3 className="text-xl font-poke lg:text-[22px]">{item.name}</h3>
     </div>
